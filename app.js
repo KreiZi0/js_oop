39f6cb8654148c6dec9e509d314f53179e12945a
KreiZi0 <steven.mirontsuk1@gmail.com>
2021-10-18 12:42:55 +0300

    1. testi ülesande tulemus

---
+++ app.js
@@ -0,0 +1,32 @@
+class Shape {
+  constructor(name, sides, sideLength) {
+    this.name = name;
+    this.sides = sides;
+    this.sideLength = sideLength;
+  }
+  calcPerimeter() {
+    return this.sides * this.sideLength;
+  }
+}
+
+class Square extends Shape {
+  calcPerimeter(){
+    return this.sideLength * this.sides
+  }
+}
+
+class Triangle extends Shape {
+  calcPerimeter(){
+    return this.sideLength * this.sides
+  }
+}
+
+const square = new Square ("square", 4, 5);
+const triangle = new Triangle("triangle", 3, 3);
+
+console.log(square.calcPerimeter());
+console.log(triangle.calcPerimeter());
+
+
+
+
