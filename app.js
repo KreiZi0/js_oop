204f63a2b82e3b672a6f6854850d0dff8b3c6d5f
KreiZi0 <steven.mirontsuk1@gmail.com>
2021-10-20 09:06:34 +0300

    3. ülesande tulemus

--- app.js
+++ app.js
@@ -7,26 +7,19 @@ class Shape {
   calcPerimeter() {
     return this.sides * this.sideLength;
   }
-}
-
-class Square extends Shape {
-  calcPerimeter(){
-    return this.sideLength * this.sides
+  calcArea() {
+    return this.sideLength * this.sideLength
   }
 }
-
-class Triangle extends Shape {
+class Square extends Shape {
   calcPerimeter(){
-    return this.sideLength * this.sides
+    return this.sideLength * this.sides 
   }
 }
 
 const square = new Square ("square", 4, 5);
-const triangle = new Triangle("triangle", 3, 3);
+
+
 
 console.log(square.calcPerimeter(), "- ruudu ümbermõõt");
-console.log(triangle.calcPerimeter(), "- kolmnurga ümbermõõt");
-
-
-
-
+console.log(square.calcArea(), "- ruudu pindala.")
